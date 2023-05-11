e3c4e2dd1f1f54973e60d98bca988bd73f1ff91e
yifei-yao <yaoyifei0@gmail.com>
2023-05-11 15:09:55 -0400

    Initial commit

---
+++ Assets/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public Camera playerCamera;
+
+    private float inputX;
+    private float inputZ;
+    private Vector3 moveDirection;
+    private CharacterController controller;
+    private Vector2 touchStartPosition;
+    public float mouseSensitivity = 12f;
+    private float yaw = 0f;
+    private float pitch = 0f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        yaw = transform.eulerAngles.y;
+        pitch = playerCamera.transform.localEulerAngles.x;
+
+    }
+
+    private void Update()
+    {
+        HandleMobileInput();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMobileMovement();
+    }
+
+    private void HandleMobileInput()
+    {
+        float movementThreshold = 20f; // Define a threshold value for the movement
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.position.x < Screen.width / 2) // Left side of the screen for movement
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    touchStartPosition = touch.position;
+                }
+                else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+                {
+                    float touchDistance = Vector2.Distance(touchStartPosition, touch.position);
+
+                    if (touchDistance > movementThreshold)
+                    {
+                        inputX = (touch.position.x - Screen.width / 4) / (Screen.width / 4);
+                        inputZ = (touch.position.y - Screen.height / 4) / (Screen.width / 4);
+                    }
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    inputX = 0;
+                    inputZ = 0;
+                }
+            }
+            else if (touch.position.x > Screen.width / 2) // Right side of the screen for camera control
+            {
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    yaw += touch.deltaPosition.x * mouseSensitivity * Time.deltaTime;
+                    pitch -= touch.deltaPosition.y * mouseSensitivity * Time.deltaTime;
+
+                    pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+                    transform.localEulerAngles = new Vector3(0, yaw, 0);
+                    playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
+                }
+            }
+        }
+
+        // Update the movement direction
+        moveDirection = transform.TransformDirection(new Vector3(inputX, 0, inputZ));
+        moveDirection.y = 0;
+        moveDirection.Normalize();
+    }
+
+    private void HandleMobileMovement()
+    {
+        float moveSpeed = 26f; // Define the desired move speed
+        Vector3 move = moveDirection * moveSpeed * Time.fixedDeltaTime;
+        controller.Move(move);
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Makerspace iOS
+- Implement virtual NYU MakerSpace in Unity for the iOS/iPadOS platform.
+- VIP Metaverse for Education
+
+## Current Progress
+- Added makerspace scene with minimal furnitures
+- Added basic player movement and camera control
+
+## Build
+Hardware requirements (The below steps are for having both, but you may still 
+be able to build this in some other ways):
+- an iPhone/iPad
+- a Mac
+
+Software requriements:
+- Unity
+- XCode
+
+Steps:
+- Enable developer mode on your iPhone/iPad
+- Connect your device with the Mac and trust the device
+- Clone and open this project in Unity
+  - Open the top-level-scene from the asset (if not already),
+    you should be able to see the room
+  - Shift+CMD+B OR Menubar -> File -> Build Settings
+    - Select iOS platform and install tools if not already
+    - Click on Build and Run, select a build directory of your choice
+    - It should now build and redirect you to XCode
+  - In your XCode, it should display one error related to the developer profile
+    - Resolve the error by logging into your developer account 
+    - Now the XCode should build
+  - Unlock your device. It should now open the build for you.
